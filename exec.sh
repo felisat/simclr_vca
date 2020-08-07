de3ded80840675e01a6bdf65d4f418ea9aa6b9d4
Felix Sattler <felix.sattler@hhi.fraunhofer.de>
2020-08-07 13:24:56 +0200

    first commit

---
+++ exec.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=felix.sattler@hhi.fraunhofer.de
+#SBATCH --output=out/%j.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus=1
+
+
+hyperparameters=' [{
+	"dataset" : ["cifar10"], 
+	"distill_dataset" : ["stl10"],
+	"net" : ["vgg11s"],
+	
+
+	"n_clients" : [20],
+	"classes_per_client" : [0],
+	"balancedness" : [1.0],
+
+
+	"communication_rounds" : [60],
+	"participation_rate" : [0.4],
+	"local_epochs" : [20],
+	"distill_epochs" : [1],
+	"n_distill" : [null], 
+
+	
+	"batch_size" : [128],
+	"use_distillation" : [true],
+	"aggregate" : [true],
+	"compress" : [false],
+	"noise" : [false],
+	
+
+	"pretrained" : [null],
+	"save_model" : [null],
+	"log_frequency" : [-100],
+	"log_path" : ["client_is_server/"],
+	"job_id" : [['$SLURM_JOB_ID']]}]'
+
+
+
+if [[ "$HOSTNAME" == *"vca"* ]]; then # Cluster
+
+	RESULTS_PATH="/opt/small_files/"
+	DATA_PATH="/opt/in_ram_data/"
+	CHECKPOINT_PATH="/opt/checkpoints/"
+
+	echo $hyperparameters
+	source "/etc/slurm/local_job_dir.sh"
+
+	export SINGULARITY_BINDPATH="$LOCAL_DATA:/data,$LOCAL_JOB_DIR:/mnt/output,./code:/opt/code,./checkpoints:/opt/checkpoints,./results:/opt/small_files,$HOME/in_ram_data:/opt/in_ram_data"
+	singularity exec --nv $HOME/base_images/pytorch15x.sif python -u /opt/code/main.py --batch_size 1024 --epochs 1 --DATA_PATH DATA_PATH
+
+	mkdir -p results
+	cp -r ${LOCAL_JOB_DIR}/. ${SLURM_SUBMIT_DIR}/results	
+
+
+else # Local
+
+	RESULTS_PATH="results/"
+	DATA_PATH="/home/sattler/Data/PyTorch/"
+	CHECKPOINT_PATH="checkpoints/"
+
+	python -u code/main.py --batch_size 1024 --epochs 1000 --DATA_PATH DATA_PATH
+
+
+
+
+
+fi
+
+
+
+
+
+
